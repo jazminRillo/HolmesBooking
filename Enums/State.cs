2f450b76268392b73bf8ae54e8b3b0617499026c
Jeronimo Magdalena <jeronimomagdalena@MBP-de-Jeronimo.lan>
2023-07-23 17:25:46 -0300

    no delete customers

--- Enums/State.cs
+++ Enums/State.cs
@@ -1,9 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
 namespace HolmesBooking;
 
 public enum State
 {
+    [Display(Name = "Confirmada")]
     CONFIRMADA,
+    [Display(Name = "Demorada")]
     DEMORADA,
+    [Display(Name = "No Show")]
     NOSHOW,
+    [Display(Name = "Cancelada")]
     CANCELADA
+}
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum enumValue)
+    {
+        return enumValue.GetType()
+                        .GetMember(enumValue.ToString())
+                        .First()
+                        .GetCustomAttribute<DisplayAttribute>()
+                        .Name;
+    }
 }
